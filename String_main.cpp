758329be7d650b40d11d2cf546ab9b0e8d1f65f7
Jaure Nathalie <njaure@bs406-s16-22.insa-lyon.fr>
2014-12-16 18:28:28 +0100

    Modification of the main by adding  Test_class_string

--- String_main.cpp
+++ String_main.cpp
@@ -23,35 +23,21 @@
 // ===========================================================================
 //                         Declare Miscellaneous Functions
 // ===========================================================================
+void Test_class_string(String S);
 
 
 // ===========================================================================
 //                                   The Main
 // ===========================================================================
+
+
+
+
 int main(int argc, char* argv[])
 {
-  String test("Test string!");
-  String test2("Another test string!");
-  printf("%d \n", test.length());
-  printf("%d \n", test.size());
-  printf("%d \n", test.max_size());
-  printf("%d \n", test.capacity());
-  printf("%d \n", test.empty());
-  printf("%s \n", test.c_str());
-  printf("%d \n", test2.length());
-  printf("%d \n", test2.size());
-  printf("%d \n", test2.max_size());
-  printf("%d \n", test2.capacity());
-  printf("%d \n", test2.empty());
-  printf("%s \n", test2.c_str());
-  test2 = test;
-  printf("%d \n", test2.length());
-  printf("%d \n", test2.size());
-  printf("%d \n", test2.max_size());
-  printf("%d \n", test2.capacity());
-  printf("%d \n", test2.empty());
-  printf("%s \n", test2.c_str());
+  String test("");
 
+Test_class_string(test);
   return 0;
 }
 
@@ -60,4 +46,17 @@ int main(int argc, char* argv[])
 // ===========================================================================
 //                         Define Miscellaneous Functions
 // ===========================================================================
+void Test_class_string(String S)
+{
+   printf("String to test : %s \n", S.c_str());
+  printf("lenght of the string : %d \n", S.length());
+  printf("size of the string :%d \n", S.size());
+  printf("max_capacity of the string : %d \n", S.max_size());
+  printf("capacity of the string :%d \n", S.capacity());
+  printf("does the string is empty ? (y=1, n=0) :%d \n", S.empty());
+  printf("character at the position 5 in the string :%c \n", S.at(5));
+  printf("character at the position 5 in the string :%c \n", S.at(5));
+ 
+}
+
 
