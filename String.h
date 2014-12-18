2e5eb070ebbb8c47d8d602c045383d64000ff33d
Muntaner Manon <mmuntaner@bs406-s16-17.insa-lyon.fr>
2014-12-18 13:55:48 +0100

    Program functional and commented

--- String.h
+++ String.h
@@ -70,15 +70,16 @@ class String
   // =======================================================================
   //                                Operators
   // =======================================================================
-   inline char& operator[](unsigned int pos);
-   inline String& operator=(char c);
-   inline String& operator=( const char* s);
-   inline String& operator= (const String& str);
-   friend inline String operator+ (const String& lhs, const String& rhs);
-   friend inline String operator+ (const String& lhs, char rhs);
-   friend inline String operator+ (char lhs, const String& rhs);
-   friend inline String operator+ (const char* lhs,const String& rhs);
-   friend inline String operator+ (const String& lhs, const char* rhs);
+  inline char& operator[](unsigned int pos);
+  inline String& operator=(char c);
+  inline String& operator=( const char* s);
+  inline String& operator= (const String& str);
+
+  friend inline String operator+ (const String& lhs, char rhs);
+  friend inline String operator+ (char lhs, const String& rhs);
+  friend inline String operator+ (const char* lhs,const String& rhs);
+  friend inline String operator+ (const String& lhs, const char* rhs);
+  friend inline String operator+ (const String& lhs, const String& rhs);
   
 
   // =======================================================================
@@ -92,7 +93,7 @@ class String
   //                             Public Attributes
   // =======================================================================
   
- protected :
+  protected :
   
   // =======================================================================
   //                            Forbidden Constructors
@@ -126,10 +127,12 @@ class String
 // ===========================================================================
 //                              Getters' definitions
 // ===========================================================================
+//Returns the length of the string, in terms of bytes
 inline unsigned int String::length(void) const
 {
   return s_length*sizeof(char);
 }
+
 
 // "size()" is exactly the same as "length()"
 inline unsigned int String::size(void) const
@@ -137,6 +140,7 @@ inline unsigned int String::size(void) const
   return length();
 }
 
+// Returns the maximum length the string can reach
 inline const unsigned int String::max_size(void) const
 {
   return MAX_CAPACITY;
@@ -146,9 +150,7 @@ inline const unsigned int String::max_size(void) const
 // for the string,expressed in terms of bytes.
 inline unsigned int String::capacity(void) const
 {
-  
-      return s_capacity*sizeof(char);
-  
+  return s_capacity*sizeof(char);
 }
 
 // "c_str" return a pointer on a c string version of s_data 
@@ -168,24 +170,26 @@ inline const char* String::c_str(void) const
 // ===========================================================================
 //                              Setters' definitions
 // ===========================================================================
-// Requests that the string capacity be adapted to a planned change in size to a length of up to n characters.
+// Requests that the string capacity be adapted to a planned change
+// in size to a length of up to n characters.
 inline void String::reserve(unsigned int n)
-{ if(n>MAX_CAPACITY)
 {
-printf("Your string is too long, not enough capacity !\n");
-exit(EXIT_FAILURE);
-}
- else if (n > s_capacity)
-{
-  char* news_data = new char [n];
-  for(int i=0;i<s_capacity;i++)
+  if(n>MAX_CAPACITY)
     {
-     news_data[i]=s_data[i];
+      printf("Your string is too long, not enough capacity !\n");
+      exit(EXIT_FAILURE);
     }
+  else if (n > s_capacity)
+    {
+      char* news_data = new char [n];
+      for(int i=0;i<s_capacity;i++)
+	{
+	  news_data[i]=s_data[i];
+	}
      delete [] s_data;
      s_data=news_data;
      s_capacity = n;
-  }
+    }
 }
 		
 // Resizes the string to a length of n characters.
@@ -229,22 +233,22 @@ inline void String::resize (unsigned int n)
 //Returns a reference to the character at position pos in the string.
 inline char & String::operator[]( unsigned int pos )
 {
-//if the position asked is outside the lenght of the string
-if (pos<0 || pos>s_length) 
-{
-printf("out of range\n");
-exit(EXIT_FAILURE);
-}
-else
-{ 
-  return this->s_data[pos-1];
+  //if the position asked is outside the lenght of the string
+  if (pos<0 || pos>s_length) 
+    {
+      printf("out of range\n");
+      exit(EXIT_FAILURE);
+    }
+  else
+    { 
+      return this->s_data[pos-1];
+    }
 }
 
-}
 
 //Assigns a new value ('c') to the string, replacing its current contents.
- inline String& String::operator=(char c)
- {
+inline String& String::operator=(char c)
+{
   s_length=1;
   s_capacity=1;
   delete [] s_data;
@@ -254,63 +258,6 @@ else
  }
 
 
-   inline String operator+ (const String& lhs,const char* rhs )
- {
-
-  unsigned int i=0;
-  while(rhs[i]!='\0')
- {
- i++;
- }
- char* data = new char[i+lhs.s_length];
- unsigned int j;
- for (j=0; j<lhs.s_length;j++)
- {
- data [j]=lhs.s_data[j];
- }
-
- for (j=lhs.s_length; j<(i+lhs.s_length);j++)
- {
- data [j]=rhs[j-lhs.s_length];
- }
- data[i+lhs.s_length]='\0';
- String string_ret(data);
-
- delete []data;
- return string_ret;
-
-
- }
-
-
-//Returns a newly constructed string object with its value 
-//being the concatenation of the characters at the right of the operator
-inline String operator+ (const char* lhs,const String& rhs)
-{
-   unsigned int i=0;
-  while(lhs[i]!='\0')
- {
- i++;
- }
- char* data = new char[i+rhs.s_length];
- unsigned int j;
- for (j=0; j<i;j++)
- {
- data[j]=lhs[j];
- }
- for (j=i; j<(i+rhs.s_length);j++)
- {
- data[j]=rhs.s_data[j-i];
- }
- data[i+rhs.s_length]='\0';
- String string_ret(data);
- delete []data;
- return string_ret;
-  
-}
-
-       
- 
 //Assigns a new value ("foo") to the string, replacing its current contents.
 String& String::operator=( const char* s )
 {
@@ -334,24 +281,6 @@ String& String::operator=( const char* s )
   return *this;
 }
 
-
-//Returns a newly constructed string object with its value 
-//being the concatenation of the characters in lhs followed by those of rhs.
-inline String operator+ (const String& lhs, const String& rhs)
-{
-  unsigned int n = lhs.s_length;
-  unsigned int m = n + rhs.s_length;
-  String newstring(lhs);
-  newstring.resize(m);
-  unsigned int j;
-  for (j=n; j<(m);j++)
-    {
-      newstring.s_data[j]=rhs.s_data[j-n];
-    }
-  newstring.s_data[m]='\0';
-  return newstring;
-  
-}
 
 // This operator compare if the String are the same, if they arent, it
 // copy str
@@ -389,19 +318,19 @@ inline String& String::operator= (const String& str)
 // This operator have two declaration, the first is used when the String is at 
 // the left of the operator. It return a concatenate String from anothe String
 // and a char
-  inline String operator+ (const String& lhs, char rhs)
-  {
-    int i;
-    char * cstr= new char [lhs.s_length+2];
-    for(i=0;i<lhs.s_length;i++)
-      {
-	cstr[i]=lhs.s_data[i];
-      }
-    cstr[i]=rhs;
-    cstr[i+1]='\0';
-    String result(cstr);
-    return result;
-  }
+inline String operator+ (const String& lhs, char rhs)
+{
+  int i;
+  char * cstr= new char [lhs.s_length+2];
+  for(i=0;i<lhs.s_length;i++)
+    {
+      cstr[i]=lhs.s_data[i];
+    }
+  cstr[i]=rhs;
+  cstr[i+1]='\0';
+  String result(cstr);
+  return result;
+}
 
 // This one is used when the String is at the right of the operator
 inline String operator+ (char lhs, const String& rhs)
@@ -417,6 +346,82 @@ inline String operator+ (char lhs, const String& rhs)
   String result(cstr);
   return result;
 }
+
+
+//Returns a newly constructed string object with its value 
+//being the concatenation of the characters at the left of the operator
+inline String operator+ (const String& lhs,const char* rhs )
+{
+  unsigned int i=0;
+  while(rhs[i]!='\0')
+    {
+      i++;
+    }
+
+  char* data = new char[i+lhs.s_length];
+  unsigned int j;
+  for (j=0; j<lhs.s_length;j++)
+    {
+      data [j]=lhs.s_data[j];
+    }
+  for (j=lhs.s_length; j<(i+lhs.s_length);j++)
+    {
+      data [j]=rhs[j-lhs.s_length];
+    }
+  data[i+lhs.s_length]='\0';
+  String string_ret(data);
+
+  delete []data;
+  return string_ret;
+}
+
+//Returns a newly constructed string object with its value 
+//being the concatenation of the characters at the right of the operator
+inline String operator+ (const char* lhs,const String& rhs)
+{
+  unsigned int i=0;
+  while(lhs[i]!='\0')
+    {
+      i++;
+    }
+  char* data = new char[i+rhs.s_length];
+  unsigned int j;
+  for (j=0; j<i;j++)
+    {
+      data[j]=lhs[j];
+    }
+  for (j=i; j<(i+rhs.s_length);j++)
+    {
+      data[j]=rhs.s_data[j-i];
+    }
+  data[i+rhs.s_length]='\0';
+  String string_ret(data);
+  delete []data;
+  return string_ret; 
+}
+    
+
+//Returns a newly constructed string object with its value 
+//being the concatenation of the characters in lhs followed by those of rhs.
+inline String operator+ (const String& lhs, const String& rhs)
+{
+  unsigned int n = lhs.s_length;
+  unsigned int m = n + rhs.s_length;
+  String newstring(lhs);
+  newstring.resize(m);
+  unsigned int j;
+  for (j=n; j<(m);j++)
+    {
+      newstring.s_data[j]=rhs.s_data[j-n];
+    }
+  newstring.s_data[m]='\0';
+  return newstring; 
+}
+
+
+
+
+
 
 
 
--- String_main.cpp
+++ String_main.cpp
@@ -36,62 +36,52 @@ void Test_class_string(String S);
 int main(int argc, char* argv[])
 {
 
-String test("Test for the string ");
-String test2("Stringtest2");
-String test3("Stringtest3");
+  String test("String to test ");
+  String test1("Stringtest1 ");
+  String test2("Stringtest2");
+  String test3(" Stringtest3");
 
-//Definition of the string to test
-Test_class_string(test);
-
-
-printf("\nTesting operators\n");
-
-//Testing the operator =(String)
-test=test2;
-printf("New string : %s \n", test.c_str());
-
-//Testing the operator =(char c)
-test='c';
-printf("New string : %s \n", test.c_str());
-
-//Testing the operator =(char *)
-test="newchar*";
-printf("New string : %s \n", test.c_str());
-
-//Testing the operator +(String)
-test=test2+test3;
-printf("New string : %s \n", test.c_str());
-
-//Testing the operator +(String)
-test=test3+test2;
-printf("New string : %s \n", test.c_str());
-
-//Testing the operator +(char c)
-test=test2+'c';
-printf("New string : %s \n", test.c_str());
+  //Definition of the string to test
+  printf("You can use the method Test_class_string to acces at the different parameters. \n");
+  Test_class_string(test);
 
 
+  printf("\nTesting operators = \n");
 
-//Testing the operator +(char *)
-test= "newchar*"+test3;
-printf("New string : %s \n", test.c_str());
+  //Testing the operator =(String)
+  test=test1;
+  printf("New string : %s \n", test.c_str());
 
-//Testing the operator +(char c)
-test='c'+test2;
-printf("New string : %s \n", test.c_str());
+  //Testing the operator =(char c)
+  test='c';
+  printf("New string : %s \n", test.c_str());
 
-//Testing the operator +(char *)
- test=test2+"newchar*";
- printf("New string : %s \n", test.c_str());
+  //Testing the operator =(char *)
+  test="newchar*";
+  printf("New string : %s \n", test.c_str());
 
 
+  printf("\nTesting operators + \n");
 
-/*
-printf("\nTesting the method clear\n");
-test.clear();
-//verification that the string is clear
-Test_class_string(test);
-*/
+  //Testing the operator +(String)
+  test=test1+test2+test3;
+  printf("New string : %s \n", test.c_str());
+
+  //Testing the operator +(char c)
+  test=test2+'c';
+  printf("New string : %s \n", test.c_str());
+
+  //Testing the operator +(char *)
+  test= "newchar1*"+ test3 + " newchar2*";
+  printf("New string : %s \n", test.c_str());
+
+
+  printf("\nTesting the method clear \n");
+
+  test.clear();
+  //verification that the string is clear
+  Test_class_string(test);
+
 
   return 0;
 }
@@ -103,14 +93,14 @@ Test_class_string(test);
 // ===========================================================================
 void Test_class_string(String S)
 {
-   printf("String to test : %s \n", S.c_str());
+  printf("String to test : %s \n", S.c_str());
   printf("Lenght of the string : %d \n", S.length());
   printf("Size of the string :%d \n", S.size());
   printf("Max_capacity of the string : %d \n", S.max_size());
   printf("Capacity of the string :%d \n", S.capacity());
   printf("Does the string is empty ? (y=1, n=0) :%d \n", S.empty());
   printf("Character at the position 5 in the string :%c \n", S.at(5));
-  printf("Character at the position 7 in the string using operator []:%c \n", S[10]);
+  printf("Character at the position 8 in the string using operator []:%c \n", S[8]);
  
 }
 
